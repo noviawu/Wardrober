8490d572fdc1b18c3bf6262dbff7ad56c3792e51
noviawu <xiaodanwu@brandeis.edu>
2021-09-30 17:47:12 -0400

    add flexbox layout to Home.js as placeholders for clothing items

--- screens/Home.js
+++ screens/Home.js
@@ -12,7 +12,43 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Home Screen is here, yet to implemented...</Text>
+      <View
+        style={{ flex: 1, flexDirection: "row", backgroundColor: "lightgreen" }}
+      >
+        <View style={{ flex: 1, backgroundColor: "lightgreen" }}>
+          <View style={{ flex: 1, backgroundColor: "pink" }}>
+            <Text style={{ fontSize: 24 }}> pink</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: "green" }}>
+            <Text style={{ fontSize: 24 }}> green</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: "yellow" }}>
+            <Text style={{ fontSize: 24 }}> yellow</Text>
+          </View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={{ flex: 1, backgroundColor: "red" }}>
+            <Text style={{ fontSize: 24 }}> red</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: "white" }}>
+            <Text style={{ fontSize: 24 }}> white</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: "blue" }}>
+            <Text style={{ fontSize: 24 }}> blue</Text>
+          </View>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "lightgreen" }}>
+          <View style={{ flex: 1, backgroundColor: "teal" }}>
+            <Text style={{ fontSize: 24 }}> teal</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: "purple" }}>
+            <Text style={{ fontSize: 24 }}> purple</Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: "silver" }}>
+            <Text style={{ fontSize: 24 }}> silver</Text>
+          </View>
+        </View>
+      </View>
       <Button
         title="Add an item"
         onPress={() => {
@@ -30,6 +66,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  items: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "yellow",
+  },
 });
 
 export default Home;
+
+/**
+ * next step:
+ * create fake data in a file that contains the image(?), and the name, and brand
+ * use MAP to loop through the data to show items.
+ *
+ */
